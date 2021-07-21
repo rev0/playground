442fd43f8401f3515a75a940901cf38abe3d720a
rev0 <cezary.szubinski@gmail.com>
2021-07-21 11:16:10 +0200

    Update pyspark.sh
    Spark & Hadoop version update

--- pyspark.sh
+++ pyspark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #default values for pyspark and SPARK_HOME
-PYSPARK="3.1.1"
-SPARKHOME="/content/spark-3.1.1-bin-hadoop2.7"
+PYSPARK="3.1.2"
+SPARKHOME="/content/spark-3.1.2-bin-hadoop3.2"
 
 while getopts v: option
 do
@@ -18,25 +18,25 @@ apt-get purge -y openjdk-11* -qq > /dev/null && sudo apt-get autoremove -y -qq >
 apt-get install -y openjdk-8-jdk-headless -qq > /dev/null
 
 if [[ "$PYSPARK" == "3.1"* ]]; then
-  wget -q "https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" > /dev/null
-  tar -xvf spark-3.1.1-bin-hadoop2.7.tgz > /dev/null
-  SPARKHOME="/content/spark-3.1.1-bin-hadoop2.7"
+  wget -q "https://downloads.apache.org/spark/spark-3.1.2/spark-3.1.2-bin-hadoop3.2.tgz" > /dev/null
+  tar -xvf spark-3.1.2-bin-hadoop3.2.tgz > /dev/null
+  SPARKHOME="/content/spark-3.1.2-bin-hadoop3.2"
 elif [[ "$PYSPARK" == "3.0"* ]]; then
-  wget -q "https://downloads.apache.org/spark/spark-3.0.2/spark-3.0.2-bin-hadoop2.7.tgz" > /dev/null
-  tar -xvf spark-3.0.2-bin-hadoop2.7.tgz > /dev/null
-  SPARKHOME="/content/spark-3.0.2-bin-hadoop2.7"
+  wget -q "https://downloads.apache.org/spark/spark-3.0.2/spark-3.0.3-bin-hadoop3.2.tgz" > /dev/null
+  tar -xvf spark-3.0.3-bin-hadoop3.2.tgz > /dev/null
+  SPARKHOME="/content/spark-3.0.3-bin-hadoop3.2"
 elif [[ "$PYSPARK" == "2"* ]]; then
   wget -q "https://downloads.apache.org/spark/spark-2.4.7/spark-2.4.7-bin-hadoop2.7.tgz" > /dev/null
   tar -xvf spark-2.4.7-bin-hadoop2.7.tgz > /dev/null
   SPARKHOME="/content/spark-2.4.7-bin-hadoop2.7"
 else
-  wget -q "https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz" > /dev/null
-  tar -xvf spark-3.1.1-bin-hadoop2.7.tgz > /dev/null
-  SPARKHOME="/content/spark-3.1.1-bin-hadoop2.7"
+  wget -q "https://downloads.apache.org/spark/spark-3.1.2/spark-3.1.2-bin-hadoop3.2.tgz" > /dev/null
+  tar -xvf spark-3.1.2-bin-hadoop3.2.tgz > /dev/null
+  SPARKHOME="/content/spark-3.1.2-bin-hadoop3.2"
 fi
 
 export SPARK_HOME=$SPARKHOME
 export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 
 # Install pyspark
-! pip install --upgrade -q pyspark==$PYSPARK findspark
+!pip install --upgrade -q pyspark==$PYSPARK findspark
