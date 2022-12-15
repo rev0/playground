28012022cdd1cf9f741b9e163d69409b306593fb
rev0 <cezary.szubinski@gmail.com>
2022-12-15 16:14:30 +0100

    Hope it's last today

--- pyspark.sh
+++ pyspark.sh
@@ -19,8 +19,8 @@ apt-get install -y openjdk-8-jdk-headless -qq > /dev/null
 
 if [[ "$PYSPARK" == "3.1"* ]]; then
   wget -q "https://downloads.apache.org/spark/spark-3.1.3/spark-3.1.3-bin-hadoop3.2.tgz" > /dev/null
-  tar -xvf spark-3.1.2-bin-hadoop3.2.tgz > /dev/null
-  SPARKHOME="/content/spark-3.1.2-bin-hadoop3.2"
+  tar -xvf spark-3.1.3-bin-hadoop3.2.tgz > /dev/null
+  SPARKHOME="/content/spark-3.1.3-bin-hadoop3.2"
 elif [[ "$PYSPARK" == "3.0"* ]]; then
   wget -q "https://downloads.apache.org/spark/spark-3.0.2/spark-3.0.3-bin-hadoop3.2.tgz" > /dev/null
   tar -xvf spark-3.0.3-bin-hadoop3.2.tgz > /dev/null
@@ -31,7 +31,7 @@ elif [[ "$PYSPARK" == "2"* ]]; then
   SPARKHOME="/content/spark-2.4.7-bin-hadoop2.7"
 else
   wget -q "https://downloads.apache.org/spark/spark-3.1.3/spark-3.1.3-bin-hadoop3.2.tgz" > /dev/null
-  tar -xvf spark-3.1.2-bin-hadoop3.2.tgz > /dev/null
+  tar -xvf spark-3.1.3-bin-hadoop3.2.tgz > /dev/null
   SPARKHOME="/content/spark-3.1.3-bin-hadoop3.2"
 fi
 
