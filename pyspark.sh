bee648a8091e975c747d08bb6d4c054ae66a1aee
rev0 <cezary.szubinski@gmail.com>
2024-07-05 00:58:18 +0200

    Update pyspark.sh

--- pyspark.sh
+++ pyspark.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #default values for pyspark and SPARK_HOME
-PYSPARK="3.1.3"
-SPARKHOME="/content/spark-3.1.3-bin-hadoop3.2"
+PYSPARK="3.5.1"
+SPARKHOME="/content/spark-3.5.1-bin-hadoop3"
 
 while getopts v: option
 do
@@ -17,10 +17,10 @@ apt-get update
 apt-get purge -y openjdk-11* -qq > /dev/null && sudo apt-get autoremove -y -qq > /dev/null
 apt-get install -y openjdk-8-jdk-headless -qq > /dev/null
 
-if [[ "$PYSPARK" == "3.1"* ]]; then
-  wget -q "https://downloads.apache.org/spark/spark-3.1.3/spark-3.1.3-bin-hadoop3.2.tgz" > /dev/null
-  tar -xvf spark-3.1.3-bin-hadoop3.2.tgz > /dev/null
-  SPARKHOME="/content/spark-3.1.3-bin-hadoop3.2"
+if [[ "$PYSPARK" == "3.5"* ]]; then
+  wget -q "https://dlcdn.apache.org/spark/spark-3.5.1/spark-3.5.1-bin-hadoop3.tgz" > /dev/null
+  tar -xvf spark-3.5.1-bin-hadoop3.tgz > /dev/null
+  SPARKHOME="/content/spark-3.5.1-bin-hadoop3"
 elif [[ "$PYSPARK" == "3.0"* ]]; then
   wget -q "https://downloads.apache.org/spark/spark-3.0.2/spark-3.0.3-bin-hadoop3.2.tgz" > /dev/null
   tar -xvf spark-3.0.3-bin-hadoop3.2.tgz > /dev/null
